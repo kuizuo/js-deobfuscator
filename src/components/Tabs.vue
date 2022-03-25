3a40d566e433f31ab4bd8a6dba531b8864df9af5
kuizuo <kuizuo12@163.com>
2022-03-25 18:20:27 +0800

    fix: 优化组件缓存

--- src/components/Tabs.vue
+++ src/components/Tabs.vue
@@ -1,49 +1,43 @@
 <script lang="ts" setup>
-import { computed, ref } from 'vue'
-import Help from './Help.vue'
-import Obfuscator from './Obfuscator.vue'
-import DeObfuscator from './DeObfuscator.vue'
-import Example from './Example/index.vue'
+import { computed, defineAsyncComponent, ref } from 'vue'
 
 const activeName = ref('deobfuscator')
-
-const tabs = ref([
+const componentKey = ref(0)
+const componentsList = [
   {
     title: 'js混淆',
     name: 'obfuscator',
+    component: defineAsyncComponent(() => import('./Obfuscator.vue')),
   },
   {
     title: 'js还原',
     name: 'deobfuscator',
+    component: defineAsyncComponent(() => import('./DeObfuscator.vue')),
   },
   {
     title: 'js还原示例代码',
     name: 'example',
+    component: defineAsyncComponent(() => import('./Example/index.vue')),
   },
   {
     title: '帮助说明',
     name: 'help',
+    component: defineAsyncComponent(() => import('./Help.vue')),
   },
-])
+]
 
-const componentMap = ref<any>({
-  help: Help,
-  example: Example,
-  deobfuscator: DeObfuscator,
-  obfuscator: Obfuscator,
-})
+const isComponent = computed(() => componentsList.find((c) => c.name === activeName.value)?.component)
 </script>
 
 <template>
   <el-tabs type="border-card" v-model="activeName">
-    <el-tab-pane v-for="item in tabs" :key="item.name" :label="item.title" :name="item.name">
+    <el-tab-pane v-for="item in componentsList" :key="item.name" :label="item.title" :name="item.name">
       <keep-alive>
-        <component :is="componentMap[activeName]"></component>
+        <component :is="isComponent" v-if="activeName === item.name"></component>
       </keep-alive>
     </el-tab-pane>
   </el-tabs>
 </template>
-
 
 <style lang="scss" scoped>
 .el-tabs {

e005116af887e78a7e6537623eb33006c0576b54
kuizuo <kuizuo12@163.com>
2022-07-22 05:18:39 +0800

    chore(deps): update deps

--- vite.config.ts
+++ vite.config.ts
@@ -4,10 +4,12 @@ import Vue from '@vitejs/plugin-vue'
 import inject from '@rollup/plugin-inject'
 import AutoImport from 'unplugin-auto-import/vite'
 import Components from 'unplugin-vue-components/vite'
+import Layouts from 'vite-plugin-vue-layouts'
 import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
 import Pages from 'vite-plugin-pages'
 import WindiCSS from 'vite-plugin-windicss'
 import PurgeIcons from 'vite-plugin-purge-icons'
+import Inspect from 'vite-plugin-inspect'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -25,28 +27,39 @@ export default defineConfig({
   },
   plugins: [
     Vue({
-      // reactivityTransform: true,
+      include: [/\.vue$/, /\.md$/],
+      reactivityTransform: true,
     }),
     // https://github.com/hannoeru/vite-plugin-pages
     Pages({
-      importMode: 'sync',
+      extensions: ['vue', 'md'],
     }),
-    Components({
-      resolvers: [ElementPlusResolver()],
-      dts: 'src/components.d.ts',
-    }),
+
+    // https://github.com/JohnCampionJr/vite-plugin-vue-layouts
+    Layouts(),
+
+    // https://github.com/antfu/unplugin-auto-import
     AutoImport({
-      resolvers: [ElementPlusResolver()],
       imports: ['vue', 'vue/macros', 'vue-router', '@vueuse/core'],
       dts: 'src/auto-import.d.ts',
+      dirs: ['src/composables', 'src/store'],
+      vueTemplate: true,
     }),
+
+    Components({
+      resolvers: [ElementPlusResolver()],
+      // allow auto load markdown components under `./src/components/`
+      extensions: ['vue', 'md'],
+      // allow auto import and register components used in markdown
+      include: [/\.vue$/, /\.vue\?vue/, /\.md$/],
+      dts: 'src/components.d.ts',
+    }),
+
     WindiCSS(),
     PurgeIcons({}),
+    Inspect(),
   ],
   build: {
-    rollupOptions: {
-      plugins: [inject({ Buffer: ['buffer', 'Buffer'] })],
-    },
     commonjsOptions: {
       transformMixedEsModules: true,
     },

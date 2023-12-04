3afb34abb07b61f910bf13e554572c94a86a5375
Kuizuo <hi@kuizuo.cn>
2023-12-05 07:48:23 +0800

    feat(play): obfuscator code

---
+++ play/obfuscator/index.js
@@ -0,0 +1,66 @@
+import { promises as fs } from 'node:fs'
+import { dirname } from 'node:path'
+import { fileURLToPath } from 'node:url'
+import { Deob } from '@deob/utils'
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = dirname(__filename)
+
+class MyDeOb extends Deob { }
+
+; (async function () {
+  const fileName = 'code'
+
+  const rawCode = await fs.readFile(`${__dirname}/${fileName}.js`, {
+    encoding: 'utf-8',
+  })
+
+  const deob = new MyDeOb(rawCode, {
+    dir: __dirname,
+    isWriteFile: true,
+  })
+
+  await deob.prettierCode()
+
+  deob.removeSelfCallFn()
+
+  let index = 0
+
+  deob.nestedFnReplace()
+
+  deob.splitMultipleDeclarations()
+  await deob.record(fileName, ++index)
+
+  const decryptFnCode = deob.findDecryptFnByBigArr(60, true)
+
+  deob.designDecryptFn(deob.decryptFnList)
+  deob.decryptReplace(decryptFnCode)
+  await deob.record(fileName, ++index)
+
+  for (let i = 1; i <= 1; i++) {
+    deob.decryptReplace()
+
+    for (let j = 1; j <= 3; j++) {
+      deob.saveAllObject()
+      deob.objectMemberReplace()
+      deob.switchFlat()
+      deob.calcBinary()
+    }
+  }
+
+  await deob.record(fileName, ++index)
+
+  // 通用处理
+  deob.replaceConstant()
+  deob.calcBinary()
+
+  deob.removeUnusedBlock()
+  deob.removeUnusedVariables()
+  deob.selfCallFnReplace()
+
+  deob.deleteExtra()
+  deob.markComment()
+
+  const code = deob.getCode()
+  await fs.writeFile(`${__dirname}/output.js`, code)
+})()
---
+++ play/obfuscator/output.js
@@ -0,0 +1,127 @@
+function hi() {
+  function _0x588477(_0x1781d6, _0x73ebed, _0x504684, _0x5f0168) {
+    return _0x4b57(_0x1781d6 - 841, _0x73ebed);
+  }
+
+  var _0x31a68e = {
+    "zoWtj": "VQpJl",
+    "SGFdI": "FoKQm"
+  };
+
+  var _0x13918c = function () {
+    var _0x5601ab = {};
+
+    function _0x48bde4(_0x38aa9b, _0x237d6c, _0x563ee1, _0x4f2bde) {
+      return _0x4b57(_0x38aa9b - 582 - 243, _0x237d6c);
+    }
+
+    _0x5601ab["UywnN"] = _0x31a68e["zoWtj"], _0x5601ab["AjDZu"] = _0x31a68e["SGFdI"];
+
+    function _0x294cfa(_0x95d0a4, _0x2b4d9c, _0x1a5bbe, _0x54c4d3) {
+      return _0x4b57(_0x1a5bbe - -1624 - 841, _0x54c4d3);
+    }
+
+    var _0x7159b6 = true;
+    return function (_0x330efc, _0x46a7b3) {
+      var _0x7fa39c = _0x7159b6 ? function () {
+        function _0x25a8b0(_0x21187d, _0x52f148, _0x4048b3, _0x436085) {
+          return _0x4b57(_0x4048b3 - 787, _0x21187d);
+        }
+
+        function _0x39f18d(_0x19097e, _0x10c36f, _0x1ea555, _0x487882) {
+          return _0x4b57(_0x487882 - -321, _0x10c36f);
+        }
+
+        if (_0x5601ab["UywnN"] !== _0x5601ab["AjDZu"]) {
+          if (_0x46a7b3) {
+            var _0x3d6c1e = _0x46a7b3["apply"](_0x330efc, arguments);
+
+            return _0x46a7b3 = null, _0x3d6c1e;
+          }
+        } else _0x22db6b = _0x4bbd53;
+      } : function () {};
+
+      return _0x7159b6 = false, _0x7fa39c;
+    };
+  }();
+
+  function _0x557424(_0x132859, _0x324157, _0x5094ec, _0xc47d7a) {
+    return _0x4b57(_0xc47d7a - 243, _0x324157);
+  }
+
+  var _0x6cbcff = _0x13918c(this, function () {
+    var _0x2bba98 = function () {
+      function _0x270a64(_0x2008c0, _0x4dc7d9, _0x1b1ac9, _0x1ba7a3) {
+        return _0x4b57(_0x4dc7d9 - 1647 - -1069 - 243, _0x2008c0);
+      }
+
+      function _0x1d94f8(_0x565f85, _0x220072, _0x579013, _0x464588) {
+        return _0x4b57(_0x579013 - 1027 - -1069 - 243, _0x220072);
+      }
+
+      var _0x6f0388;
+
+      try {
+        if (true) _0x6f0388 = Function("return (function() {}.constructor(\"return this\")( ));")();else {
+          var _0x5a6f56 = _0x40defb ? function () {
+            function _0x8ea9c4(_0x28725d, _0x534eb0, _0x5dcbcd, _0x57e070) {
+              return _0x4b57(_0x57e070 - 58 - 1027 - -1069 - 243, _0x5dcbcd);
+            }
+
+            if (_0x24e3b2) {
+              var _0x3f7b59 = _0x237167["apply"](_0x4b4d1b, arguments);
+
+              return _0x269724 = null, _0x3f7b59;
+            }
+          } : function () {};
+
+          return _0x542fef = false, _0x5a6f56;
+        }
+      } catch (_0x10880c) {
+        if (false) {
+          var _0x5f7acf = _0x506f16["constructor"]["prototype"]["bind"](_0x7d4867);
+
+          var _0xc56e0 = _0x4ed3a6[_0x1d3f2a];
+
+          var _0x4982de = _0x3457ce[_0xc56e0] || _0x5f7acf;
+
+          _0x5f7acf["__proto__"] = _0x25f351["bind"](_0x592499), _0x5f7acf["toString"] = _0x4982de["toString"]["bind"](_0x4982de), _0x2b5296[_0xc56e0] = _0x5f7acf;
+        } else _0x6f0388 = window;
+      }
+
+      return _0x6f0388;
+    };
+
+    function _0x5c7c8e(_0x3f0f84, _0x5e4578, _0x410143, _0x1ce8db) {
+      return _0x4b57(_0x410143 - -1069 - 243, _0x1ce8db);
+    }
+
+    var _0x45bfdc = _0x2bba98();
+
+    var _0x5d2030 = _0x45bfdc["console"] = _0x45bfdc["console"] || {};
+
+    var _0x4a4cd0 = ["log", "warn", "info", "error", "exception", "table", "trace"];
+
+    function _0x38b7d1(_0x3f27ff, _0x38444a, _0x45b3f8, _0x32c69e) {
+      return _0x4b57(_0x38444a - -254 - 841, _0x45b3f8);
+    }
+
+    for (var _0xbcb23b = 904 + 154 * -5 + -134; _0xbcb23b < _0x4a4cd0["length"]; _0xbcb23b++) {
+      var _0x4af79f = 7917 + -7917;
+
+      var _0x12c43a = _0x13918c["constructor"]["prototype"]["bind"](_0x13918c);
+
+      var _0x545650 = _0x4a4cd0[_0xbcb23b];
+
+      var _0x1faa06 = _0x5d2030[_0x545650] || _0x12c43a;
+
+      _0x12c43a["__proto__"] = _0x13918c["bind"](_0x13918c);
+      _0x12c43a["toString"] = _0x1faa06["toString"]["bind"](_0x1faa06);
+      _0x5d2030[_0x545650] = _0x12c43a;
+    }
+  });
+
+  _0x6cbcff(), console["log"]("Hello World!");
+}
+
+hi();

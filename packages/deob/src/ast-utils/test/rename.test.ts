25f67a5513e7544858d72cc75d955aec08d6b5c9
Kuizuo <hi@kuizuo.cn>
2026-02-01 17:25:46 +0800

    chore: lint code

--- packages/deob/src/ast-utils/test/rename.test.ts
+++ packages/deob/src/ast-utils/test/rename.test.ts
@@ -1,38 +1,38 @@
-import { parse } from '@babel/parser';
-import traverse from '@babel/traverse';
-import { describe, expect, test } from 'vitest';
-import { renameFast, renameParameters } from '../rename';
+import { parse } from '@babel/parser'
+import traverse from '@babel/traverse'
+import { describe, expect, test } from 'vitest'
+import { renameFast, renameParameters } from '../rename'
 
 describe('rename variable', () => {
   test('conflict with existing binding', () => {
-    const ast = parse('let a = 1; let b = a;');
+    const ast = parse('let a = 1; let b = a;')
     traverse(ast, {
       Program(path) {
-        const binding = path.scope.getBinding('a')!;
-        renameFast(binding, 'b');
+        const binding = path.scope.getBinding('a')!
+        renameFast(binding, 'b')
 
-        expect(path.scope.bindings).keys('b', '_b');
+        expect(path.scope.bindings).keys('b', '_b')
       },
-    });
+    })
     expect(ast).toMatchInlineSnapshot(`
       let b = 1;
       let _b = b;
-    `);
-  });
+    `)
+  })
 
   test('duplicate function binding', () => {
-    const ast = parse('var a; function a() {}');
+    const ast = parse('var a; function a() {}')
     traverse(ast, {
       Program(path) {
-        const binding = path.scope.getBinding('a')!;
-        renameFast(binding, 'b');
+        const binding = path.scope.getBinding('a')!
+        renameFast(binding, 'b')
       },
-    });
+    })
     expect(ast).toMatchInlineSnapshot(`
       var b;
       function b() {}
-    `);
-  });
+    `)
+  })
 
   test('different types of assignments', () => {
     const ast = parse(`
@@ -46,13 +46,13 @@ describe('rename variable', () => {
       for (a of []);
       for (a in []);
       delete a;
-    `);
+    `)
     traverse(ast, {
       Program(path) {
-        const binding = path.scope.getBinding('a')!;
-        renameFast(binding, 'b');
+        const binding = path.scope.getBinding('a')!
+        renameFast(binding, 'b')
       },
-    });
+    })
     expect(ast).toMatchInlineSnapshot(`
       var b = 1;
       var b = 2;
@@ -66,36 +66,36 @@ describe('rename variable', () => {
       for (b of []);
       for (b in []);
       delete b;
-    `);
-  });
-});
+    `)
+  })
+})
 
 describe('rename parameters', () => {
   test('fewer than specified', () => {
-    const ast = parse('function f(a, b, c) { a + b + c;}');
+    const ast = parse('function f(a, b, c) { a + b + c;}')
     traverse(ast, {
       Function(path) {
-        renameParameters(path, ['x', 'y']);
+        renameParameters(path, ['x', 'y'])
       },
-    });
+    })
     expect(ast).toMatchInlineSnapshot(`
       function f(x, y, c) {
         x + y + c;
       }
-    `);
-  });
+    `)
+  })
 
   test('more than specified', () => {
-    const ast = parse('function f(a, b, c) { a + b + c;}');
+    const ast = parse('function f(a, b, c) { a + b + c;}')
     traverse(ast, {
       Function(path) {
-        renameParameters(path, ['x', 'y', 'z', 'w']);
+        renameParameters(path, ['x', 'y', 'z', 'w'])
       },
-    });
+    })
     expect(ast).toMatchInlineSnapshot(`
       function f(x, y, z) {
         x + y + z;
       }
-    `);
-  });
-});
+    `)
+  })
+})
--- packages/deob/src/deobfuscate/inline-decoder-wrappers.ts
+++ packages/deob/src/deobfuscate/inline-decoder-wrappers.ts
@@ -1,7 +1,7 @@
-import type { NodePath } from '@babel/traverse';
-import type * as t from '@babel/types';
-import type { Transform } from '../ast-utils';
-import { inlineFunctionAliases, inlineVariableAliases } from '../ast-utils';
+import type { NodePath } from '@babel/traverse'
+import type * as t from '@babel/types'
+import type { Transform } from '../ast-utils'
+import { inlineFunctionAliases, inlineVariableAliases } from '../ast-utils'
 
 /**
  * Replaces all references to `var alias = decode;` with `decode`
@@ -11,13 +11,13 @@ export default {
   tags: ['unsafe'],
   scope: true,
   run(ast, state, decoder) {
-    if (!decoder?.node.id) return;
+    if (!decoder?.node.id) return
 
-    const decoderName = decoder.node.id.name;
-    const decoderBinding = decoder.parentPath.scope.getBinding(decoderName);
+    const decoderName = decoder.node.id.name
+    const decoderBinding = decoder.parentPath.scope.getBinding(decoderName)
     if (decoderBinding) {
-      state.changes += inlineVariableAliases(decoderBinding).changes;
-      state.changes += inlineFunctionAliases(decoderBinding).changes;
+      state.changes += inlineVariableAliases(decoderBinding).changes
+      state.changes += inlineFunctionAliases(decoderBinding).changes
     }
   },
-} satisfies Transform<NodePath<t.FunctionDeclaration>>;
+} satisfies Transform<NodePath<t.FunctionDeclaration>>

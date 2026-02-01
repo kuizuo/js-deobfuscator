97194d0e2ad261568eec3e273e503b41f5594255
Kuizuo <hi@kuizuo.cn>
2026-02-01 14:49:32 +0800

    refactor: update evalCode function to support sandbox execution and improve error handling in deobfuscation process

--- packages/deob/src/transforms/test/decoder.test.ts
+++ packages/deob/src/transforms/test/decoder.test.ts
@@ -1,6 +1,8 @@
 import { parse } from '@babel/parser'
 import { describe, expect, it } from 'vitest'
-import { evalCode, generate } from '../..'
+import { evalCode } from '../..'
+import { generate } from '../../ast-utils'
+import { createNodeSandbox } from '../../deobfuscate'
 import { decodeStrings } from '../decode-strings'
 import { findDecoderByArray } from '../find-decoder-by-array'
 import { findDecoderByCallCount } from '../find-decoder-by-call-count'
@@ -16,10 +18,11 @@ describe('decoder', async () => {
       decoder("ZGVidWdnZXI=")
       `)
 
+    const sandbox = createNodeSandbox()
     const { decoders, setupCode } = findDecoderByCallCount(ast, 2)
 
-    evalCode(setupCode)
-    decodeStrings(decoders)
+    evalCode(sandbox, setupCode)
+    decodeStrings(sandbox, decoders)
 
     decoders.forEach(d => d.path.remove())
 
@@ -43,10 +46,12 @@ describe('decoder', async () => {
       decoder(1)
       `)
 
+    const sandbox = createNodeSandbox()
+
     const { stringArray, decoders, rotators, setupCode } = findDecoderByArray(ast)
 
-    evalCode(setupCode)
-    decodeStrings(decoders)
+    evalCode(sandbox, setupCode)
+    decodeStrings(sandbox, decoders)
 
     stringArray?.path.remove()
     decoders.forEach(d => d.path.remove())
